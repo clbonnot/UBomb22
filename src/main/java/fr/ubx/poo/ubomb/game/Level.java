72bdbfcf0b433fba0b3cf101f1026699cfcf4749
lbrouet <lukycyti@gmail.com>
2022-11-08 16:50:57 +0100

    1er commit

--- src/main/java/fr/ubx/poo/ubomb/game/Level.java
+++ src/main/java/fr/ubx/poo/ubomb/game/Level.java
@@ -70,7 +70,7 @@ public class Level implements Grid {
 
     @Override
     public boolean inside(Position position) {
-        return true;
+        return position.x() >= 0 && position.x() < width && position.y() >= 0 && position.y() < height;
     }
 
     @Override
