2064e06b6b8ee6abfc147c988688dc9a03276b8e
Bonnot Clement <clement.bonnot@etu.u-bordeaux.fr>
2023-01-03 00:52:57 +0100

    add monster timer fix bug

--- src/main/java/fr/ubx/poo/ubomb/go/character/Monster.java
+++ src/main/java/fr/ubx/poo/ubomb/go/character/Monster.java
@@ -14,7 +14,7 @@ public class Monster extends GameCharacter {
         super(game, position);
         this.lives = lives;
         invincibility = false;
-        this.timerInvincibility = new Timer(1000);
+        timerInvincibility = new Timer(1000);
     }
 
     public void update(long now) {
